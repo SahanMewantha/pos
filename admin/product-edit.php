315b2d4800030dce36d5ae5c1475e0864df44ac6
Sahan Mewantha <149218702+SahanMewantha@users.noreply.github.com>
2024-04-13 11:41:04 +0530

    update product display

--- admin/product-edit.php
+++ admin/product-edit.php
@@ -12,7 +12,20 @@
     <div class="card-body">
     <?php alertMessage(); ?>
         <form action="./code.php" method="post">
-            <div class="row">
+
+        <?php
+            $paramValue=checkParamId('id');
+            if(!is_numeric($paramValue)){
+
+                echo '<h5> id is no integer </h5>';
+                return false;
+            }
+            $product=getById('products',$paramValue); 
+            if($product){
+                if($product['status'] ==200){
+                    ?>
+
+           <div class="row">
                 <div class="col-md-12 mb-3">
                     <label>Select Category</label>
                     <select name="category_id" class="form-select">
@@ -37,34 +50,44 @@
                 </div>
                 <div class="col-md-12 mb-3">
                     <label for="">Product Name  *</label>
-                    <input type="text" name="name" placeholder="" required class="form-control"/>
+                    <input type="text" name="name" placeholder="" required value="<?= $product['data']['name'] ?>" class="form-control"/>
                 </div>
 
                 <div class="col-md-6 mb-3">
                     <label for="">Product Price  *</label>
-                    <input type="number" name="price" placeholder="" required class="form-control"/>
+                    <input type="number" name="price" placeholder="" required value="<?= $product['data']['price'] ?>" class="form-control"/>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="">Quntity  *</label>
-                    <input type="number" name="quntity" placeholder="" required class="form-control"/>
+                    <input type="number" name="quntity" placeholder="" required value="<?= $product['data']['quntity'] ?>" class="form-control"/>
                 </div>
 
-                <div class="col-md-12 mb-3">
-                    <label for="">Description</label>
-                    <textarea name="description" class="form-control" rows="3"></textarea>
-                </div>
                 <div class="col-md-6">
                     <label for="">Status (Unchecked=Visible ,Checked=Hidden) </label>
                     <br>
-                    <input type="checkbox" name="status" style="width:30px; height:30px;">
+                    <input type="checkbox" name="status" value="<?= $product['data']['status']==true ? 'checked':'';  ?>" style="width:30px; height:30px;">
                 </div>
 
 
                 <div class="col-md-12  text-end">
                     <br>
-                    <button type="submit" name="saveProduct" class="btn btn-primary">Save</button>
+                    <button type="submit" name="updateProduct" class="btn btn-primary">Save</button>
                 </div>
             </div>
+        <?php
+
+            }
+            else{
+                echo '<h5>' .$product['message']. '</h5>'; 
+            }
+            }
+            else{
+            echo '<h5> Somthing went wrong ! </h5>';
+            return false; 
+            }                
+
+
+        ?>
         </form>
 </div>
 
