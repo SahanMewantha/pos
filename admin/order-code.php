41bdf0f3873f2ad288a93aceae2285927606feef
Sahan Mewantha <149218702+SahanMewantha@users.noreply.github.com>
2024-04-15 18:19:19 +0530

    oder 2

--- admin/order-code.php
+++ admin/order-code.php
@@ -5,5 +5,30 @@
     {
         $productid=validate($_POST['product_id']);
         $quntity=validate($_POST['quntity']);
+        
+        $cheackProduct=mysqli_query($conn,"SELECT * FROM products WHERE id='$productid' LIMIT 1");
+        if($cheackProduct){
+            if(mysqli_num_rows($cheackProduct)>0){
+                $row=mysqli_fetch_assoc($cheackProduct);
+                if($row['quntity']<$quntity){
+                    redirct('orders-create.php','Only' .$row['quntity']. 'quntity avilable !');  
+                }
+                $productData=[
+                    'product_id' => $row['id'],
+                    'name'=> $row['name'],
+                    'price'=> $row['rice'],
+                    'quntity'=> $row['quntity']
+                ];
+                array_push($_SESSION['productItemId'],$row['id']);
+                array_push($_SESSION['productItem'],$productData);
+            }
+            else{
+                redirct('orders-create.php','No product found !');  
+            }
+
+        }else{
+            redirct('orders-create.php','Somthing went wrong !');
+        }
+
     }
 ?>
