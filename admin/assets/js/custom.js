3f8caa27ee08d0a081d425562fcf919614d72713
Sahan Mewantha <149218702+SahanMewantha@users.noreply.github.com>
2024-04-19 10:05:20 +0530

    item inc & dec

--- admin/assets/js/custom.js
+++ admin/assets/js/custom.js
@@ -1,3 +1,32 @@
 $(document).ready(function (){
-    alert('hi');
+
+    $(document).on('click', '.increment' ,function(){
+        var $quntityInput =$(this).closest('.qtyBox').find('.qty');
+        var productId =$(this).closest('.qtyBox').find('.prodId');
+
+        var curruntValue =parseInt($quntityInput.val());
+
+        if(!isNaN(curruntValue)){
+            var qtyVal=curruntValue+1;
+            $quntityInput.val(qtyVal);
+        }
+
+
+    });
+
+    $(document).on('click', '.decrement' ,function(){
+        var $quntityInput =$(this).closest('.qtyBox').find('.qty');
+        var productId =$(this).closest('.qtyBox').find('.prodId');
+
+        var curruntValue =parseInt($quntityInput.val());
+
+        if(!isNaN(curruntValue) && curruntValue > 1){
+            var qtyVal=curruntValue-1;
+            $quntityInput.val(qtyVal);
+        }
+
+
+    });
+
+
 });
