1c21d3281f31dd51113ec0fe24cb584e4d500e32
Sahan Mewantha <149218702+SahanMewantha@users.noreply.github.com>
2024-05-05 18:37:25 +0530

    display order

--- admin/orders.php
+++ admin/orders.php
@@ -4,11 +4,61 @@
     <div class="card mt-4 shadow">
         <div class="card-header">
                 <h4 class="mb-0">Orders
-                <a href="product.php" class="btn btn-danger float-end">Back</a>
+        
                 </h4>        
         </div>
         <div class="card-body">
+            <?php
+                $query="SELECT o.*,c.* FROM orders o, customers c WHERE c.id=o.customer_id ORDER BY o.id DESC";
+                $orders =mysqli_query($conn,$query);
+                if($orders)
+                {
+                    if(mysqli_num_rows($orders)>0)
+                    {
+                        ?>
+                            <table class="table table-striped table-bordered align-items-center justify-content-center">
+                                <thead>
+                                    <tr>
+                                        <th>Tracking No</th>
+                                        <th>C Name</th>
+                                        <th>C Phone</th>
+                                        <th>Order Date</th>
+                                        <th>Order Status</th>
+                                        <th>Payment Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php foreach($orders as $ordersitem): ?>
+                                    <tr>
+                                        <td class="fw-bold"><?= $ordersitem['tracking_no'];?></td>
+                                        <td><?= $ordersitem['cusname'];?></td>
+                                        <td><?= $ordersitem['phone'];?></td>
+                                        <td><?= date('d M, Y',strtotime($ordersitem['order_date']));?></td>
+                                        <td><?= $ordersitem['order_status'];?></td>
+                                        <td><?= $ordersitem['payment_mode'];?></td>
+                                        <td>
+                                            <a href="" class="btn btn-info mb-0 px-2 btn-sm">View</a>
+                                            <a href="" class="btn btn-primary mb-0 px-2 btn-sm">Print</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                                </tbody>
+                            </table>
 
+                        <?php
+
+                    }
+                    else{
+                        echo "<h5>No Records Found<h5>";
+                    }
+
+                }
+                else{
+
+                }
+            ?>
+            
         </div>
     </div>
 </div>
