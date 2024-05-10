e99f711c099e5ebc23b577a9243b176f301081f2
Sahan Mewantha <149218702+SahanMewantha@users.noreply.github.com>
2024-05-10 11:37:54 +0530

    order querys

--- admin/orders-print.php
+++ admin/orders-print.php
@@ -3,11 +3,35 @@
 <div class="container-fluid px-4">
     <div class="card mt-4 shadow">
         <div class="card-header">
-                <h4 class="mb-0">Orders
+                <h4 class="mb-0">Print Order
+                <a href="orders.php" class="btn btn-danger float-end">Back</a>
         
                 </h4>        
         </div>
         <div class="card-body">
+            <?php
+                if($_GET['track']){
+
+                    $trackingNo = validate($_GET['track']);
+                    if($trackingNo== ''){
+                    ?>
+                        <div class="text-center py-5">
+                            <h5>Please provide tracking Number!</h5>
+                            <div>
+                            <a href="orders.php" class="btn btn-primary mt-4 w-25">Go Back to Oders</a>
+                            </div>
+                        
+                        </div>
+                    <?php
+                    }
+
+                    $orderQuery ="";
+
+                }else{
+
+                }
+            
+            ?>
 
         
         </div>
